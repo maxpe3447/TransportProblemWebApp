42c5d7004e220f778380293b2e3e56cb0005ce43
maxpe3447 <121520054+mentorAC@users.noreply.github.com>
2023-01-29 11:04:06 +0200

    add validation

---
+++ TransportProblemLib/Validation.cs
@@ -0,0 +1,31 @@
+﻿namespace TransportProblemLib
+{
+    public static class Validation
+    {
+        public static bool IsUpdate(ref double[] needs, ref double[] reserves)
+        {
+            double reservesSum = reserves.Sum();
+            double needsSum = needs.Sum();
+
+            if (reservesSum == needsSum)
+            {
+                return false;
+            }
+            if (reservesSum > needsSum)
+            {
+                UpdateArray(ref needs, needsSum, reservesSum);
+            }
+            else
+            {
+                UpdateArray(ref reserves, needsSum, reservesSum);
+            }
+            return true;
+        }
+
+        private static void UpdateArray(ref double[] lst, double value1, double value2)
+        {
+            Array.Resize(ref lst, lst.Length + 1);
+            lst[lst.Length - 1] = Math.Abs(value1 - value1);
+        }
+    }
+}
