437ef76e376129e5772216a813d3c5692fa4cd9d
maxpe3447 <121520054+mentorAC@users.noreply.github.com>
2023-01-29 11:03:20 +0200

    add extension

---
+++ TransportProblemLib/Extention/TransportAlgorithmExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using TransportProblemLib.Abstruct;
+
+namespace TransportProblemLib.Extention
+{
+    public static class TransportAlgorithmExtension
+    {
+        public static double GetSum(this TransportAlgorithm alg, double[,] plan, double[,] prices)
+        {
+            double sum = 0;
+            for (int i = 0; i < plan.Length; i++)
+            {
+                int j = (i - i % alg.Needs.Length) / alg.Needs.Length;
+                int k = i % alg.Needs.Length;
+                if (plan[j, k] == plan[j, k])
+                    sum += plan[j, k] * prices[j, k];
+            }
+            return sum;
+        }
+    }
+}
